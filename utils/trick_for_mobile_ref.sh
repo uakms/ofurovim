6ef35d44f5ce8633bf1153fcd3062c91c9a63dfa
nakinor <nakinor@gmail.com>
2017-11-03 22:31:48 +0900

    catch up with the original

--- utils/trick_for_mobile_ref.sh
+++ utils/trick_for_mobile_ref.sh
@@ -1,6 +1,6 @@
 # Author: nakinor
 # Created: 2017-08-08
-# Revised: 2017-08-08
+# Revised: 2017-10-22
 
 TAG_A='<meta name="viewport" content="width=device-width, initial-scale=1.0" />'
 TAG_B='</pre>'
@@ -9,9 +9,12 @@ TAG_C='<pre class="menu-comment">'
 STR_A='<title>目次 (Vim Reference Manual)</title>'
 STR_B='ユーザーマニュアルでは編集作業の進め方を説明します。'
 STR_C='リファレンスマニュアルでは Vim の詳細を説明します。'
+STR_D='&bull; '
+STR_E='\&bull;'
 
 # ファイルの置き換えができるのは GNU sed だけ
 # 文字列にスラッシュ / が含まれているので、区切り文字はバーティカル | で
 sed -i -e "1,10s|$STR_A|$STR_A\n$TAG_A|" $1
 sed -i -e "1,200s|$STR_B|$TAG_B$STR_B$TAG_C|" $1
 sed -i -e "1,300s|$STR_C|$TAG_B$STR_C$TAG_C|" $1
+sed -i -e "s|$STR_D|$STR_E|" $1
